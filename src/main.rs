47f037392cc14a377dfc128e688ad24a84667a9f
reiden <hollyholly2014@outlook.jp>
2025-04-29 17:25:42 +0900

    change from rppal to serialport

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,68 @@
-use bytes::{BytesMut, BufMut, Bytes, Buf};
-use log::{info, debug, error, warn};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use log::{debug, error, info, warn};
+use serialport::{DataBits, SerialPort, StopBits};
+use std::error::Error;
 use std::fs::OpenOptions;
 use std::io;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{channel, Receiver};
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
-use std::{net::SocketAddr, io::Read, io::Write};
-use std::error::Error;
 use std::time::Duration;
-use std::sync::mpsc::{channel, Receiver};
+use std::{io::Read, io::Write, net::SocketAddr};
 
-use env_logger::{
-    Builder,
-    Env, Target,
-};
+use env_logger::{Builder, Env, Target};
 use prometheus_exporter::prometheus::register_gauge;
-use rppal::uart::{Parity, Uart, Queue};
 
 mod parser;
-use parser::{parser, PanDesc, IpAddr};
+use parser::{parser, IpAddr, PanDesc};
 mod command;
 use command::Command;
 mod echonet_lite;
 
-use crate::parser::{Response};
-use crate::echonet_lite::{EchonetLite, EData, EDataFormat1, EOJ_HOUSING_LOW_VOLTAGE_SMART_METER, EDataProperty, EpcLowVoltageSmartMeter};
-
+use crate::echonet_lite::{
+    EData, EDataFormat1, EDataProperty, EchonetLite, EpcLowVoltageSmartMeter,
+    EOJ_HOUSING_LOW_VOLTAGE_SMART_METER,
+};
+use crate::parser::Response;
 
 #[derive(Debug)]
 struct UartReader {
-    inner: Arc<Mutex<Uart>>,
+    inner: Arc<Mutex<Box<dyn SerialPort>>>,
     is_closed: Arc<AtomicBool>,
 }
 
 #[derive(Debug)]
 struct UartWriter {
-    inner: Arc<Mutex<Uart>>,
+    inner: Arc<Mutex<Box<dyn SerialPort>>>,
     is_closed: Arc<AtomicBool>,
 }
 
-fn split_uart(uart: Uart) -> (UartReader, UartWriter) {
+fn split_uart(uart: Box<dyn SerialPort>) -> (UartReader, UartWriter) {
     let inner = Arc::new(Mutex::new(uart));
     let is_closed = Arc::new(AtomicBool::new(false));
-    (UartReader { inner: inner.clone(), is_closed: is_closed.clone() }, UartWriter { inner, is_closed })
+    (
+        UartReader {
+            inner: inner.clone(),
+            is_closed: is_closed.clone(),
+        },
+        UartWriter { inner, is_closed },
+    )
 }
 
 impl Read for UartReader {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         if self.is_closed.load(Ordering::Acquire) {
-            return Err(io::Error::new(io::ErrorKind::Other, "uart writer is disconnected"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "uart writer is disconnected",
+            ));
         }
-        self.inner.lock().expect("failed to acuire lock").read(buf).map_err(|e| 
-            io::Error::new(io::ErrorKind::Other, e)
-        )
+        self.inner
+            .lock()
+            .expect("failed to acuire lock")
+            .read(buf)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
     }
 }
 
@@ -75,21 +85,31 @@ impl UartWriter {
 impl Write for UartWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         if self.is_closed.load(Ordering::Acquire) {
-            return Err(io::Error::new(io::ErrorKind::Other, "uart reader is disconnected"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "uart reader is disconnected",
+            ));
         }
 
-        self.inner.lock().expect("failed to acuire lock").write(buf).map_err(|e| 
-            io::Error::new(io::ErrorKind::Other, e)
-        )
+        self.inner
+            .lock()
+            .expect("failed to acuire lock")
+            .write(buf)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
     }
 
     fn flush(&mut self) -> io::Result<()> {
         if self.is_closed.load(Ordering::Acquire) {
-            return Err(io::Error::new(io::ErrorKind::Other, "uart reader is disconnected"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "uart reader is disconnected",
+            ));
         }
-        self.inner.lock().expect("failed to acuire lock").flush(Queue::Both).map_err(|e| 
-            io::Error::new(io::ErrorKind::Other, e)
-        )
+        self.inner
+            .lock()
+            .expect("failed to acuire lock")
+            .flush()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
     }
 }
 
@@ -99,10 +119,13 @@ impl Drop for UartWriter {
     }
 }
 
-fn active_scan(sensor: &mut UartWriter, receiver: &mut Receiver<Response>) -> Result<PanDesc, Box<dyn Error>> {
+fn active_scan(
+    sensor: &mut UartWriter,
+    receiver: &mut Receiver<Response>,
+) -> Result<PanDesc, Box<dyn Error>> {
     sensor.send_command(Command::ActiveScan { duration: 6 })?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkScan { ..}) {
+    if !matches!(r, Response::SkScan { .. }) {
         return Err("SKSCAN failed".into());
     }
 
@@ -117,14 +140,16 @@ fn active_scan(sensor: &mut UartWriter, receiver: &mut Receiver<Response>) -> Re
             }
             Response::EPanDesc(pandesc) => {
                 tmp = Ok(pandesc);
-            },
-            _ => {
             }
+            _ => {}
         }
     }
 }
 
-fn wait_for_connect(sensor: &mut UartWriter, receiver: &mut Receiver<Response>) -> Result<(), Box<dyn Error>> {
+fn wait_for_connect(
+    sensor: &mut UartWriter,
+    receiver: &mut Receiver<Response>,
+) -> Result<(), Box<dyn Error>> {
     let total_wait_time = Duration::from_millis(0);
     loop {
         if total_wait_time > Duration::from_secs(30) {
@@ -135,21 +160,23 @@ fn wait_for_connect(sensor: &mut UartWriter, receiver: &mut Receiver<Response>) 
         match r {
             Response::Event { num: 0x24, .. } => {
                 return Err("failed to connect to PANA".into());
-            },
+            }
             Response::Event { num: 0x25, .. } => {
                 return Ok(());
             }
-            _ => {
-            }
+            _ => {}
         }
     }
 }
 
-fn send_initialize_command_sequence(writer: &mut UartWriter, receiver: &mut Receiver<Response>) -> Result<IpAddr, Box<dyn Error>> {
+fn send_initialize_command_sequence(
+    writer: &mut UartWriter,
+    receiver: &mut Receiver<Response>,
+) -> Result<IpAddr, Box<dyn Error>> {
     // reset
     writer.send_command(Command::SkReset)?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkReset) {
+    if !matches!(r, Response::SkReset) {
         return Err("SKRESET failed".into());
     }
 
@@ -157,14 +184,14 @@ fn send_initialize_command_sequence(writer: &mut UartWriter, receiver: &mut Rece
     writer.send_command(Command::SkSetRbid { id: B_ID })?;
     let r = receiver.recv()?;
 
-    if ! matches!(r, Response::SkSetRbid { ..}) {
+    if !matches!(r, Response::SkSetRbid { .. }) {
         return Err("SKSETRBID failed".into());
     }
 
     // send pw
     writer.send_command(Command::SkSetPwd { pwd: B_PW })?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkSetPwd { ..} ) {
+    if !matches!(r, Response::SkSetPwd { .. }) {
         return Err("SKSETPWD failed".into());
     }
 
@@ -172,21 +199,29 @@ fn send_initialize_command_sequence(writer: &mut UartWriter, receiver: &mut Rece
     debug!("pan_desc: {:?}", pan_desc);
 
     // set channel
-    writer.send_command(Command::SkSreg { sreg: 0x02, val: pan_desc.channel as u32 })?;
+    writer.send_command(Command::SkSreg {
+        sreg: 0x02,
+        val: pan_desc.channel as u32,
+    })?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkSreg { ..} ) {
+    if !matches!(r, Response::SkSreg { .. }) {
         return Err("SKSREG failed".into());
     }
 
     // set pan id
-    writer.send_command(Command::SkSreg { sreg: 0x03, val: pan_desc.pan_id as u32 })?;
+    writer.send_command(Command::SkSreg {
+        sreg: 0x03,
+        val: pan_desc.pan_id as u32,
+    })?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkSreg { ..} ) {
+    if !matches!(r, Response::SkSreg { .. }) {
         return Err("SKSREG failed".into());
     }
 
     // convert addr
-    writer.send_command(Command::SkLl64 { addr64: &pan_desc.addr })?;
+    writer.send_command(Command::SkLl64 {
+        addr64: &pan_desc.addr,
+    })?;
     let r = receiver.recv()?;
     let ipv6_addr = match r {
         Response::SkLl64 { ipaddr, .. } => ipaddr,
@@ -198,7 +233,7 @@ fn send_initialize_command_sequence(writer: &mut UartWriter, receiver: &mut Rece
     // connect to pana
     writer.send_command(Command::SkJoin { ipaddr: &ipv6_addr })?;
     let r = receiver.recv()?;
-    if ! matches!(r, Response::SkJoin { ..} ) {
+    if !matches!(r, Response::SkJoin { .. }) {
         return Err("SKJOIN failed".into());
     }
 
@@ -207,18 +242,21 @@ fn send_initialize_command_sequence(writer: &mut UartWriter, receiver: &mut Rece
     Ok(ipv6_addr)
 }
 
-
 // # cancellation
 // It is caller responsibility to ensure that the previous reader thread closes before calling initialize again.
 // By dropping thre writer, reader.read() will get error and then the reader thread closes.
 // Note that reader.read() yield something no later than reader timeout set by uart.set_read_mode().
 // So, if you drop the writer, you can successfully join the reader thread within the timeout.
-fn initialize() -> Result<(UartWriter, Receiver<Response>, IpAddr, JoinHandle<()>), Box<dyn Error>>  {
-    let mut uart = Uart::with_path("/dev/ttyAMA0", 115200, Parity::None, 8, 1)?;
-
-    // Configure read() to block until at least 1 byte is received or timeout elapsed
-    uart.set_read_mode(0, Duration::from_millis(2000))?;
-    uart.set_write_mode(true)?;
+fn initialize() -> Result<(UartWriter, Receiver<Response>, IpAddr, JoinHandle<()>), Box<dyn Error>>
+{
+    
+    let mut uart = serialport::new("/dev/ttyS1", 115200)
+    .parity(serialport::Parity::None)
+    .data_bits( DataBits::Eight)
+    .stop_bits(StopBits::One)
+    .timeout(Duration::from_millis(2000))
+    .open()
+    .expect("Failed to open serial port");
 
     let (sender, mut receiver) = channel();
     let (mut reader, mut writer) = split_uart(uart);
@@ -232,7 +270,7 @@ fn initialize() -> Result<(UartWriter, Receiver<Response>, IpAddr, JoinHandle<()
                 Ok(n) if n > 0 => {
                     debug!("read: {:?}", &b[..n]);
                     buf.put(&b[..n]);
-                },
+                }
                 Err(e) => {
                     error!("uart read error: {:?}", e);
                     break;
@@ -247,11 +285,11 @@ fn initialize() -> Result<(UartWriter, Receiver<Response>, IpAddr, JoinHandle<()
                     sender.send(line).unwrap();
 
                     buf = BytesMut::from(rest);
-                },
+                }
                 Err(nom::Err::Incomplete(n)) => {
                     // not enough data
                     debug!("parse incomplate: {:?}", n);
-                },
+                }
                 Err(e) => {
                     error!("parse error: {:?}", e);
 
@@ -269,20 +307,18 @@ fn initialize() -> Result<(UartWriter, Receiver<Response>, IpAddr, JoinHandle<()
         Err(e) => {
             drop(writer);
             handle.join().expect("failed to join the reader thread");
-            return Err(e)
+            return Err(e);
         }
     };
 
     Ok((writer, receiver, ipv6_addr, handle))
 }
 
-
 const B_ID: &str = std::env!("B_ID");
 const B_PW: &str = std::env!("B_PW");
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let env = Env::default()
-        .default_filter_or("debug");
+    let env = Env::default().default_filter_or("debug");
     let mut builder = Builder::from_env(env);
 
     if let Ok(dest) = std::env::var("RUST_LOG_DESTINATION") {
@@ -302,16 +338,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     let exporter = prometheus_exporter::start(addr).expect("can not start exporter");
     let duration = std::time::Duration::from_millis(10000);
 
-    let counter_error_initialize = register_gauge!("counter_error_initialize", "# of error when try to initialize sensor with PANA")
-        .expect("can not create gauge counter_error_initialize");
-    let counter_error_sksendto = register_gauge!("counter_error_sksendto", "# of error when sending data to sensor")
-        .expect("can not create gauge counter_error_sksendto");
-    let counter_success_initialize = register_gauge!("counter_success_initialize", "# of times client finished initialization")
-        .expect("can not create gauge counter_success_initialize");
-    let counter_request_energy = register_gauge!("counter_request_energy", "# of times client send energy request")
-        .expect("can not create gauge counter_request_energy");
-    let instantaneous_energy = register_gauge!("instantaneous_energy", "Current Power Consumption in Watt")
-        .expect("can not create gauge instantaneous_energy");
+    let counter_error_initialize = register_gauge!(
+        "counter_error_initialize",
+        "# of error when try to initialize sensor with PANA"
+    )
+    .expect("can not create gauge counter_error_initialize");
+    let counter_error_sksendto = register_gauge!(
+        "counter_error_sksendto",
+        "# of error when sending data to sensor"
+    )
+    .expect("can not create gauge counter_error_sksendto");
+    let counter_success_initialize = register_gauge!(
+        "counter_success_initialize",
+        "# of times client finished initialization"
+    )
+    .expect("can not create gauge counter_success_initialize");
+    let counter_request_energy = register_gauge!(
+        "counter_request_energy",
+        "# of times client send energy request"
+    )
+    .expect("can not create gauge counter_request_energy");
+    let instantaneous_energy =
+        register_gauge!("instantaneous_energy", "Current Power Consumption in Watt")
+            .expect("can not create gauge instantaneous_energy");
 
     loop {
         let (mut writer, mut receiver, ipv6_addr, handle) = match initialize() {
@@ -335,7 +384,6 @@ fn main() -> Result<(), Box<dyn Error>> {
                 break 'main;
             }
             counter_request_energy.inc();
-    
 
             // wait response for energy request
             'wait_response: loop {
@@ -347,23 +395,28 @@ fn main() -> Result<(), Box<dyn Error>> {
                     }
                 };
                 info!("got response {:?}", r);
-    
+
                 match r {
-                    Response::SkSendTo{ result: 0x00, .. } => {
+                    Response::SkSendTo { result: 0x00, .. } => {
                         debug!("send energy request success");
-                    },
-                    Response::SkSendTo{ result: _, .. } => {
+                    }
+                    Response::SkSendTo { result: _, .. } => {
                         warn!("failed to send energy request: {:?}", r);
                         counter_error_sksendto.inc();
                         break 'wait_response;
-                    },
+                    }
                     Response::ERxUdp {
-                        data: EchonetLite {
-                            edata: EData::EDataFormat1(EDataFormat1 {
-                                seoj: EOJ_HOUSING_LOW_VOLTAGE_SMART_METER,
-                                props,
+                        data:
+                            EchonetLite {
+                                edata:
+                                    EData::EDataFormat1(EDataFormat1 {
+                                        seoj: EOJ_HOUSING_LOW_VOLTAGE_SMART_METER,
+                                        props,
+                                        ..
+                                    }),
                                 ..
-                        }), .. }, ..
+                            },
+                        ..
                     } => {
                         for prop in props {
                             match prop {
@@ -375,14 +428,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                                 } => {
                                     let power = edt.get_u32();
                                     instantaneous_energy.set(power as f64);
-                                },
+                                }
                                 _ => {
                                     // ignore
                                 }
                             }
                         }
                         break 'wait_response;
-                    },
+                    }
                     _ => {
                         // ignore
                     }
